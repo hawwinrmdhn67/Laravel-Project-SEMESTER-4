1264b137a90e3b4833d8a57929f369e916a5ec80
hawwinrmdhn67 <hawwinxstar@gmail.com>
2026-02-23 10:27:56 +0700

    Menambahkan halaman about

---
+++ laravel12-app/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+// fitur baru halaman about
+Route::get('/about', function () {
+    return Inertia::render('Halaman About');
+})->name('about');
+
+Route::get('dashboard', function () {
+    return Inertia::render('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/settings.php';
